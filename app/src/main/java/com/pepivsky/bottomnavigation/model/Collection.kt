ba2e531908ed4949eba3ef16361a979efa0bc727
Pepivsky <ironglas68@gmail.com>
2020-12-15 21:48:33 -0600

    Fragments agregados

--- app/src/main/java/com/pepivsky/bottomnavigation/model/Collection.kt
+++ app/src/main/java/com/pepivsky/bottomnavigation/model/Collection.kt
@@ -1,11 +1,21 @@
 package com.pepivsky.bottomnavigation.model
 
-import android.media.Image
+import java.util.Locale.filter
+
 
 data class Collection(val tittle: String, val listCard: MutableList<FlashCard>)
 
 //object statico
 object Collections {
     val collectionsList = mutableListOf<Collection>()
+    /*fun obtenerRespuestas(index: Int): List<FlashCard> {
+        val listaRespuestas = mutableListOf<FlashCard>()
+
+       *//* val flashCard = collectionsList.filter{ (indice, calificacion) -> indice != index }[0]
+        listaRespuestas = Collection.filter{ calificacion -> calificacion != index}
+
+
+        return*//*
+    }*/
 }
 
